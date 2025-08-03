bd7fd6d44641584609affe6a5a90537b71aa75fe
Richard Theuws <richard@theuws.com>
2025-08-03 10:32:24 +0200

    Updated LanguageContext.tsx

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -168,25 +168,6 @@ export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ chil
     </LanguageContext.Provider>
   );
 };
-  }, [language]);
-
-  const t = (key: string): string => {
-    const keys = key.split('.');
-    let value: any = translations[language];
-    
-    for (const k of keys) {
-      value = value?.[k];
-    }
-    
-    return value || key;
-  };
-
-  return (
-    <LanguageContext.Provider value={{ language, setLanguage, t }}>
-      {children}
-    </LanguageContext.Provider>
-  );
-};
 
 export const useLanguage = () => {
   const context = useContext(LanguageContext);
